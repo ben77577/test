81aae9a5c48aefbcfa2e8f423a91ebe50b3894f9
Ben Pardon <pardonbk5336@uwec.edu>
2020-05-16 09:36:36 -0500

    test

---
+++ precache-manifest.2e4b720661e2af9d914f8a789462e4b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a1188a1b2c2582648f38baee558f11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09dd5a7cde2d1cf5c692",
+    "url": "/static/css/main.1a17b751.chunk.css"
+  },
+  {
+    "revision": "57d039ee655919e4c7fd",
+    "url": "/static/js/2.a9179ab0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9179ab0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09dd5a7cde2d1cf5c692",
+    "url": "/static/js/main.dfeb7eea.chunk.js"
+  },
+  {
+    "revision": "ac13e59a201b6ccd2062",
+    "url": "/static/js/runtime-main.d34f0c14.js"
+  }
+]);
